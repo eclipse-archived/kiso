66f664e12bb14c14c2c69b0e0ca5abc5b07af9f8
amushatov <50954548+amushatov@users.noreply.github.com>
2019-07-26 19:11:39 +0300

    Add MbedTLS to Kiso (#156)

---
+++ boards/CommonGateway/kiso_config/MbedTLS/threading_alt.h
@@ -0,0 +1,28 @@
+#ifndef MBEDTLS_THREADING_ALT_H
+#define MBEDTLS_THREADING_ALT_H
+
+#include <stdlib.h>
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "semphr.h"
+#include "task.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct
+{
+    SemaphoreHandle_t mutex;
+} mbedtls_threading_mutex_t;
+
+void threading_mutex_init(mbedtls_threading_mutex_t *mutex);
+void threading_mutex_free(mbedtls_threading_mutex_t *mutex);
+int threading_mutex_lock(mbedtls_threading_mutex_t *mutex);
+int threading_mutex_unlock(mbedtls_threading_mutex_t *mutex);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MBEDTLS_THREADING_ALT_H */
