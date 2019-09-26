e10290c54b22858c0a866f393a427721fa8ad5b8
Yong Shen <47750708+yongbosch@users.noreply.github.com>
2019-09-26 12:34:36 +0200

    oss findings: freertos unit test headers (#287)
    * address oss findings: freertos unit test headers
    
    retain the original copyright/license header
    
    Signed-off-by: Yong Shen <yong.shen@de.bosch.com>
    
    * Update thirdparty/freertos/test/unit/include/list_th.hh
    
    Co-Authored-By: Lars Beseke <36697481+elbosch@users.noreply.github.com>
    
    * Update thirdparty/freertos/test/unit/include/task_th.hh
    
    Co-Authored-By: Lars Beseke <36697481+elbosch@users.noreply.github.com>

--- thirdparty/freertos/test/unit/include/list_th.hh
+++ thirdparty/freertos/test/unit/include/list_th.hh
@@ -1,16 +1,32 @@
-/********************************************************************************
-* Copyright (c) 2010-2019 Robert Bosch GmbH
-*
-* This program and the accompanying materials are made available under the
-* terms of the Eclipse Public License 2.0 which is available at
-* http://www.eclipse.org/legal/epl-2.0.
-*
-* SPDX-License-Identifier: EPL-2.0
-*
-* Contributors:
-*    Robert Bosch GmbH - initial contribution
-*
-********************************************************************************/
+/*
+ * FreeRTOS Kernel V10.0.1
+ * Copyright (C) 2017 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://www.FreeRTOS.org
+ * http://aws.amazon.com/freertos
+ *
+ * 1 tab == 4 spaces!
+ */
+
+/* Modified by Robert Bosch GmbH */
+/* Modification: adapt this header file to Kiso unit test header */
 
 #ifndef KISO_LIST_TH_HH_
 #define KISO_LIST_TH_HH_
